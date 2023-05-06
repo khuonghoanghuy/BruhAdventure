3b5a4b8f8e88b0ae93a9c522376a1f781dbc9132
khuonghoanghuy <khuonghoanghuy@gmail.com>
2023-05-06 15:40:54 +0700

    making options menu

--- source/OptionsMenu.hx
+++ source/OptionsMenu.hx
@@ -1,19 +1,27 @@
 package;
 
 import flixel.FlxG;
-import flixel.FlxSprite;
 import flixel.group.FlxGroup.FlxTypedGroup;
+import flixel.text.FlxText;
+import flixel.util.FlxColor;
 
 class OptionsMenu extends MainState
 {
-	var list:Array<String> = ["Exit"];
-	var group_button:FlxTypedGroup<FlxSprite>;
+	var list:Array<String> = ["Run Speed", "Exit"];
+	var group_button:FlxTypedGroup<FlxText>;
 	var curSelected:Int = 0;
 
 	override public function create()
 	{
-		group_button = new FlxTypedGroup<FlxSprite>();
+		group_button = new FlxTypedGroup<FlxText>();
 		add(group_button);
+
+		for (i in 0...list.length)
+		{
+			var optionText:FlxText = new FlxText(20, 300 + (i * 50), 0, list[i], 32);
+			optionText.ID = i;
+			group_button.add(optionText);
+		}
 
 		super.create();
 
@@ -33,6 +41,40 @@ class OptionsMenu extends MainState
 		{
 			change(1);
 		}
+
+		if (FlxG.keys.justPressed.ENTER)
+		{
+			switch (list[curSelected])
+			{
+				case "Exit":
+					FlxG.save.flush();
+					FlxG.switchState(new MenuState());
+			}
+		}
+
+		if (FlxG.keys.justPressed.ESCAPE)
+		{
+			FlxG.save.flush();
+			FlxG.switchState(new MenuState());
+		}
+
+		if (FlxG.keys.anyJustPressed([LEFT, A]))
+		{
+			switch (list[curSelected])
+			{
+				case "Run Speed":
+					FlxG.save.data.runSpeed -= 10;
+			}
+		}
+
+		if (FlxG.keys.anyJustPressed([RIGHT, D]))
+		{
+			switch (list[curSelected])
+			{
+				case "Run Speed":
+					FlxG.save.data.runSpeed += 10;
+			}
+		}
 	}
 
 	function change(change:Int = 0)
@@ -43,5 +85,13 @@ class OptionsMenu extends MainState
 			curSelected = group_button.length - 1;
 		if (curSelected >= group_button.length)
 			curSelected = 0;
+
+		group_button.forEach(function(txt:FlxText)
+		{
+			txt.color = FlxColor.WHITE;
+
+			if (txt.ID == curSelected)
+				txt.color = FlxColor.YELLOW;
+		});
 	}
 }
